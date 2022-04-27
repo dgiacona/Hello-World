e81becf05cac20eae744562ef0ff87bddfea4520
Dominic Giacona <dominic@exobyte.io>
2022-04-27 19:43:05 -0400

    added javascript for lab 6

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let userName = prompt("What is your name?");
+
+console.log(userName);
+
+if (userName === "dom") {
+    console.log("Welcome_Dom");
+} else {
+    console.log("Hello Friend! Welcome!");
+}
+
+let place = prompt("Welcome" + userName + "!What is your favorite piece of tech?");
+console.log("First response = " + place);
+
+place = prompt("What else do you like?");
+console.log("Second response = " + place);
+
+document.write("<p id='username'>" + userName + "<p>");
