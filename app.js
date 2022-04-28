9f7ecc28f1fa2eccddb221f3b82a39e23266469f
Dominic Giacona <dominic@exobyte.io>
2022-04-28 16:35:02 -0400

    js modifications

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 'use strict';
 
-let userName = prompt("What is your name?");
+function popup(message){
 
-console.log(userName);
+    let userName = prompt(message);
 
-if (userName === "dom") {
-    console.log("Welcome_Dom");
-} else {
-    console.log("Hello Friend! Welcome!");
+    console.log(userName);
+    
+    if (userName.toLowerCase() === "dom") {
+        console.log("Welcome Dom");
+    } else {
+        console.log("Hello Friend! Welcome!");
+    }
+    
+    let place = prompt("Welcome " + userName + "! What is your favorite piece of tech?");
+    console.log("First response = " + place);
+    
+    place = prompt("What else do you like?");
+    console.log("Second response = " + place);
+    
+    document.querySelector(".username").innerHTML = "<p id='username'>" + userName + "<p>";
 }
-
-let place = prompt("Welcome" + userName + "!What is your favorite piece of tech?");
-console.log("First response = " + place);
-
-place = prompt("What else do you like?");
-console.log("Second response = " + place);
-
-document.querySelector(".username").innerHTML = "<p id='username'>" + userName + "<p>";
+popup("Name Please:")
